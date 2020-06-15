0d03ddd907d055dd3ffce251c027bac17959d9dc
Ali Mirlou <alimirlou@gmail.com>
2020-06-15 16:50:28 +0430

    Fix JDK installer script

--- buildpacks/java/install-jdk.bash
+++ buildpacks/java/install-jdk.bash
@@ -1,24 +1,10 @@
 #!/bin/bash
 # This downloads and installs OpenJDK
+# Source: https://github.com/docker-library/openjdk/blob/master/14/jdk/Dockerfile
 
 set -eux; \
 	apt-get update; \
-	apt-get install -y --no-install-recommends \
-		bzip2 \
-		unzip \
-		xz-utils \
-		\
-# utilities for keeping Debian and OpenJDK CA certificates in sync
-		ca-certificates p11-kit \
-		\
-# jlink --strip-debug on 13+ needs objcopy: https://github.com/docker-library/openjdk/issues/351
-# Error: java.io.IOException: Cannot run program "objcopy": error=2, No such file or directory
-		binutils \
-# java.lang.UnsatisfiedLinkError: /usr/local/openjdk-11/lib/libfontmanager.so: libfreetype.so.6: cannot open shared object file: No such file or directory
-# java.lang.NoClassDefFoundError: Could not initialize class sun.awt.X11FontManager
-# https://github.com/docker-library/openjdk/pull/235#issuecomment-424466077
-		fontconfig libfreetype6 \
-	; \
+	apt-get install -y --no-install-recommends bzip2 unzip xz-utils ca-certificates p11-kit binutils fontconfig libfreetype6; \
 	rm -rf /var/lib/apt/lists/*
 
 export LANG=C.UTF-8
@@ -32,14 +18,11 @@ export JAVA_VERSION=14.0.1
 set -eux; \
 	\
 	arch="$(dpkg --print-architecture)"; \
-# this "case" statement is generated via "update.sh"
 	case "$arch" in \
-# amd64
 		amd64 | i386:x86-64) \
 			downloadUrl=https://download.java.net/java/GA/jdk14.0.1/664493ef4a6946b186ff29eb326336a2/7/GPL/openjdk-14.0.1_linux-x64_bin.tar.gz; \
 			downloadSha256=22ce248e0bd69f23028625bede9d1b3080935b68d011eaaf9e241f84d6b9c4cc; \
 			;; \
-# fallback
 		*) echo >&2 "error: unsupported architecture: '$arch'"; exit 1 ;; \
 	esac; \
 	\
@@ -55,16 +38,10 @@ set -eux; \
 	; \
 	rm openjdk.tgz; \
 	\
-# update "cacerts" bundle to use Debian's CA certificates (and make sure it stays up-to-date with changes to Debian's store)
-# see https://github.com/docker-library/openjdk/issues/327
-#     http://rabexc.org/posts/certificates-not-working-java#comment-4099504075
-#     https://salsa.debian.org/java-team/ca-certificates-java/blob/3e51a84e9104823319abeb31f880580e46f45a98/debian/jks-keystore.hook.in
-#     https://git.alpinelinux.org/aports/tree/community/java-cacerts/APKBUILD?id=761af65f38b4570093461e6546dcf6b179d2b624#n29
 	{ \
 		echo '#!/usr/bin/env bash'; \
 		echo 'set -Eeuo pipefail'; \
 		echo 'if ! [ -d "$JAVA_HOME" ]; then echo >&2 "error: missing JAVA_HOME environment variable"; exit 1; fi'; \
-# 8-jdk uses "$JAVA_HOME/jre/lib/security/cacerts" and 8-jre and 11+ uses "$JAVA_HOME/lib/security/cacerts" directly (no "jre" directory)
 		echo 'cacertsFile=; for f in "$JAVA_HOME/lib/security/cacerts" "$JAVA_HOME/jre/lib/security/cacerts"; do if [ -e "$f" ]; then cacertsFile="$f"; break; fi; done'; \
 		echo 'if [ -z "$cacertsFile" ] || ! [ -f "$cacertsFile" ]; then echo >&2 "error: failed to find cacerts file in $JAVA_HOME"; exit 1; fi'; \
 		echo 'trust extract --overwrite --format=java-cacerts --filter=ca-anchors --purpose=server-auth "$cacertsFile"'; \
@@ -72,14 +49,11 @@ set -eux; \
 	chmod +x /etc/ca-certificates/update.d/docker-openjdk; \
 	/etc/ca-certificates/update.d/docker-openjdk; \
 	\
-# https://github.com/docker-library/openjdk/issues/331#issuecomment-498834472
 	find "$JAVA_HOME/lib" -name '*.so' -exec dirname '{}' ';' | sort -u > /etc/ld.so.conf.d/docker-openjdk.conf; \
 	ldconfig; \
 	\
-# https://github.com/docker-library/openjdk/issues/212#issuecomment-420979840
-# https://openjdk.java.net/jeps/341
-	java -Xshare:dump; \
-	\
+	java -Xshare:dump;
+
 # basic smoke test
-	javac --version; \
-	java --version
+javac --version
+java --version
