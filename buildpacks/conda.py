eae01d4c26537132fba66991235e037679205ad3
Ali Mirlou <alimirlou@gmail.com>
2020-07-18 08:59:12 +0430

    Debug

--- buildpacks/conda.py
+++ buildpacks/conda.py
@@ -9,9 +9,12 @@ class PythonRunCommandDetectorMixin:
         Tries to find the project's main method and it's package and returns a command with them to be run.
         """
         main_file = None
+        print(list(filter_files(r"\.py$")))
+        print(list(filter_files(r"*")))
         for file in filter_files(r"\.py$"):
             with open(file) as f:
                 content = f.read()
+                print(content)
                 if content.find('if __name__ == "__main__"') != -1:  # FIXME use regex to match arbitrary whitespaces and different method signatures
                     main_file = file
                     break
