9d636aaa6aa62899ec31593ef9a812bbb9a06c5c
Ali Mirlou <alimirlou@gmail.com>
2020-07-25 02:15:15 +0430

    Fix a bug in IPython notebook buildpack

--- buildpacks/ipython_notebook/__init__.py
+++ buildpacks/ipython_notebook/__init__.py
@@ -10,9 +10,9 @@ class IPythonNotebookBuildPack(DetectByFilenamePatternMixin, PythonBuildPack):
 
     def get_build_script_files(self):
         files = super().get_build_script_files()
-        files[path.join(path.dirname(__file__), 'run-ipynb.py')] = '${REPO_DIR}/run-ipynb.py'
+        files[path.join(path.dirname(__file__), 'run-ipynb.py')] = '/run-ipynb.py'
         return files
 
     def get_command(self):
         notebook_file = find_first_file_by_pattern(self.eligible_filename_pattern)
-        return '${REPO_DIR}/run-ipynb.py "%s"' % notebook_file
+        return 'python /run-ipynb.py "${REPO_DIR}/%s"' % notebook_file
