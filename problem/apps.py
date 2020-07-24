666487629fe7da48ecf01d68d0223c34e93e80e3
Ali Mirlou <alimirlou@gmail.com>
2020-07-24 16:25:25 +0430

    Fix a bug in Kubernetes client

--- problem/apps.py
+++ problem/apps.py
@@ -7,14 +7,15 @@ from django.utils.module_loading import import_string
 class ProblemConfig(AppConfig):
     name = 'problem'
     queue = None
-    kubernetes = None
+
+    @property
+    def kubernetes(self):
+        # Initialize a kubernetes client each time this property is used because the library deletes the SSL files after some time
+        config.load_kube_config()
+        return client.CustomObjectsApi()
 
     def ready(self):
         from django.conf import settings
 
         # Initialize message queue client and assign it to app object
         self.queue = import_string(settings.QUEUE_CLIENT)(settings.QUEUE_SERVER_API_URL)
-
-        # Initialize kubernetes client and assign it to app object
-        config.load_kube_config()
-        self.kubernetes = client.CustomObjectsApi()
