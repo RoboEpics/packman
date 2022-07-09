b8526c9811999df7b062a62c614d4fce3efef5bd
Ali Mirlou <alimirlou@gmail.com>
2022-07-09 16:57:36 +0430

    Update find_python_main_file function

--- buildpacks/conda.py
+++ buildpacks/conda.py
@@ -6,11 +6,17 @@ from .base import filter_files
 
 
 def find_python_main_file():
+    count = 0
     for file in filter_files(r"\.py$"):
+        count += 1
         with open(file) as f:
             content = f.read()
             if search(r"^if\s+__name__\s*==\s*[\"']__main__[\"']\s*:", content, flags=MULTILINE) is not None:
                 return file
+
+    # If there was only one Python file in the project, return it anyway
+    if count == 1:
+        return file
 
     raise RuntimeError("Could not find main file! Aborting dockerization...")
 
