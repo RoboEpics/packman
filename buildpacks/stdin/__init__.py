94f33097e39202f1f3585dd32c6c0e7c9977bd6f
Ali Mirlou <alimirlou@gmail.com>
2022-07-09 15:47:32 +0430

    Add more stdin buildpacks

--- buildpacks/stdin/__init__.py
+++ buildpacks/stdin/__init__.py
@@ -1,4 +1,8 @@
 from .python_stdin import Python310STDINBuildPack
+from .nodejs_stdin import NodeJSSTDINBuildPack
+from .php_stdin import PHPSTDINBuildPack
+from .erlang_stdin import ErlangSTDINBuildPack
+from .cpp_stdin import CPPBuildPack
 
 
-__all__ = [Python310STDINBuildPack]
+__all__ = [Python310STDINBuildPack, NodeJSSTDINBuildPack, PHPSTDINBuildPack, ErlangSTDINBuildPack, CPPBuildPack]
