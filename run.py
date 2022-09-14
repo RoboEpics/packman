40ecb1e506f1c90acec3edc98d1ca2fd5fb0392e
Ali Mirlou <alimirlou@gmail.com>
2022-09-14 14:31:52 +0430

    Improve performance

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ logger.setLevel(log_level)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'packman.settings')
 
 import django
+
 django.setup()
 
 from django.conf import settings
@@ -67,7 +68,7 @@ def create_docker_image(gitlab_repo, commit_hash, image_name, buildpack=None):
     r2d.log_level = log_level
     r2d.repo = f"https://oauth2:{settings.GIT_ADMIN_TOKEN}@{settings.GIT_HOST}/{gitlab_repo}.git"
     r2d.ref = commit_hash
-    r2d.output_image_spec = image_name = '/'.join((settings.DOCKER_REGISTRY_HOST, image_name))
+    r2d.output_image_spec = image_name
     r2d.user_id = 2000
     r2d.user_name = 'jovyan'
     if buildpack is None:
@@ -81,9 +82,9 @@ def create_docker_image(gitlab_repo, commit_hash, image_name, buildpack=None):
     r2d.build()
 
     # run_command = r2d.picked_buildpack.get_command()
-    run_command = None
+    # run_command = None
 
-    return image_name, run_command
+    # return run_command
 
 
 def push_image_to_registry(image_name):
@@ -140,61 +141,68 @@ def handle_new_message(channel, method_frame, header_frame, result):
         client.ack(method_frame.delivery_tag)
         return
 
+    if submission.status == Submission.SubmissionStatus.WAITING_IN_QUEUE:
+        submission.status = Submission.SubmissionStatus.IMAGE_BUILD_JOB_ENQUEUED
+        submission.save()
+
     enter = submission.problem_enter
     if enter is None:
         return
 
-    # Create Docker image from Gitlab repository
-    logger.info("Creating Docker image for submission %d..." % submission.id)
+    image_name = '/'.join((settings.DOCKER_REGISTRY_HOST, submission.generate_image_name()))
+    if submission.status == Submission.SubmissionStatus.IMAGE_BUILD_JOB_ENQUEUED:
+        # Create Docker image from Gitlab repository
+        logger.info("Creating Docker image for submission %d..." % submission.id)
 
-    submission.status = Submission.SubmissionStatus.IMAGE_BUILD_STARTED
-    submission.save()
+        submission.status = Submission.SubmissionStatus.IMAGE_BUILD_STARTED
+        submission.save()
 
-    buildpack = None  # TODO this code was written in a rush for a specific competition. needs cleaning.
-    if submission.runtime:
-        logger.info("Submission has runtime " + submission.get_runtime_display())
-        if submission.runtime == "other":
-            submission.status = Submission.SubmissionStatus.SUBMISSION_READY
-            submission.save(skip_run=True)
-            logger.info("Skipping submission due to unsupported runtime...")
+        buildpack = None  # TODO this code was written in a rush for a specific competition. needs cleaning.
+        if submission.runtime:
+            logger.info("Submission has runtime " + submission.get_runtime_display())
+            if submission.runtime == "other":
+                submission.status = Submission.SubmissionStatus.SUBMISSION_READY
+                submission.save(skip_run=True)
+                logger.info("Skipping submission due to unsupported runtime...")
+                return
+            buildpack = getattr(stdin_buildpacks, submission.runtime, None)
+
+        try:
+            create_docker_image(enter.code.get_git_repo_path(), submission.reference, image_name, buildpack)
+        except Exception:
+            capture_exception()
+            logger.error("Something went wrong while building Docker image for submission %d!" % submission.id)
+
+            submission.status = Submission.SubmissionStatus.IMAGE_BUILD_FAILED
+            submission.save()
+
+            client.ack(method_frame.delivery_tag)
+
             return
-        buildpack = getattr(stdin_buildpacks, submission.runtime, None)
 
-    try:
-        image_name, run_command = create_docker_image(enter.code.get_git_repo_path(), submission.reference, submission.generate_image_name(), buildpack)
-    except Exception:
-        capture_exception()
-        logger.error("Something went wrong while building Docker image for submission %d!" % submission.id)
-
-        submission.status = Submission.SubmissionStatus.IMAGE_BUILD_FAILED
+        submission.status = Submission.SubmissionStatus.IMAGE_BUILD_SUCCESSFUL
+        # submission.command = ' '.join(run_command)
         submission.save()
 
-        client.ack(method_frame.delivery_tag)
+        logger.info("Successfully created Docker image for submission %d!" % submission.id)
 
-        return
+    if submission.status == Submission.SubmissionStatus.IMAGE_BUILD_SUCCESSFUL:
+        # Push the image to Docker registry
+        logger.info("Pushing Docker image for submission %d..." % submission.id)
+        try:
+            push_image_to_registry(image_name)
 
-    submission.status = Submission.SubmissionStatus.IMAGE_BUILD_SUCCESSFUL
-    # submission.command = ' '.join(run_command)
-    submission.save()
+            submission.status = Submission.SubmissionStatus.SUBMISSION_READY
+            submission.save()
 
-    logger.info("Successfully created Docker image for submission %d!" % submission.id)
+            logger.info("Successfully pushed Docker image for submission %d!" % submission.id)
+        except ChildProcessError as e:
+            capture_exception()
 
-    # Push the image to Docker registry
-    logger.info("Pushing Docker image for submission %d..." % submission.id)
-    try:
-        push_image_to_registry(image_name)
+            submission.status = Submission.SubmissionStatus.IMAGE_PUSH_FAILED
+            submission.save()
 
-        submission.status = Submission.SubmissionStatus.SUBMISSION_READY
-        submission.save()
-
-        logger.info("Successfully pushed Docker image for submission %d!" % submission.id)
-    except ChildProcessError as e:
-        capture_exception()
-
-        submission.status = Submission.SubmissionStatus.IMAGE_PUSH_FAILED
-        submission.save()
-
-        logger.error("Something went wrong while pushing Docker image for submission %d: %s!" % (submission.id, str(e)))
+            logger.error("Something went wrong while pushing Docker image for submission %d: %s!" % (submission.id, str(e)))
 
     # Reset operator's rating for this problem, FIXME it's better to be in a post_save signal
     # Leaderboard.objects.get(
